acf60db919f44d8071f545e6543735545307111b
ramity <lewisclbrown@gmail.com>
2025-07-30 16:29:31 -0500

    added migration coverage for the token to tokenHash delta for platformInvite entity

---
+++ src/backend/Caspnetti.DAL/Migrations/20250730211910_platform_invite_token_delta.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Caspnetti.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class platform_invite_token_delta : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Token",
+                table: "PlatformInvite",
+                newName: "TokenHash");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TokenHash",
+                table: "PlatformInvite",
+                newName: "Token");
+        }
+    }
+}
