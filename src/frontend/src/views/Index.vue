765362431fc3199b4cff9b8087de58d3a48b14ff
ramity <lewisclbrown@gmail.com>
2025-07-30 17:48:28 -0500

    added a splash screen for landing w/ login and register navigation

--- src/frontend/src/views/Index.vue
+++ src/frontend/src/views/Index.vue
@@ -1,8 +1,46 @@
 <template>
+  <div class="container-fluid">
+    <div class="row">
+      <div id="wrychain-banner" class="col vh-100 d-flex align-items-center justify-content-center">
+
+        <div class="box text-center">
+          <h1 id="wrychain-header" class="display-1 mb-5 py-4">Wrychain</h1>
+          <div class="p-5 bg-light">Trust is your social network.</div>
+        </div>
+
+      </div>
+    </div>
+    <div id="login" class="row bg-dark">
+      <router-link to="/login" class="p-0 text-decoration-none text-white">
+        <div class="col p-5 display-6">
+          Login
+        </div>
+      </router-link>
+    </div>
+    <div class="row bg-dark">
+      <router-link to="/register" class="p-0 text-decoration-none text-white">
+        <div class="col mx-0 p-5 display-6">
+          Register
+        </div>
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script>
 </script>
 
-<style>
+<style scoped>
+div#wrychain-banner
+{
+  border-bottom: 10px solid #b2bec3;
+}
+h1#wrychain-header
+{
+  border-left: 10px solid #b2bec3;
+}
+div#login
+{
+  border-bottom: 1px solid #636e72;
+}
 </style>
