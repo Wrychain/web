5bc0c6d2feeb378d03d8289a03fdce18c98b117b
ramity <lewisclbrown@gmail.com>
2025-07-30 20:22:33 -0500

    implemented registration process on userService

--- src/backend/Caspnetti.Service/UserService.cs
+++ src/backend/Caspnetti.Service/UserService.cs
@@ -6,15 +6,35 @@ namespace Caspnetti.Service;
 
 public class UserService
 {
+    private readonly PlatformInviteService _platformInviteService;
     private readonly UserRepository _userRepository;
 
     public UserService(UserRepository userRepository)
     {
+        _platformInviteService = platformInviteService;
         _userRepository = userRepository;
     }
 
-    public bool Register(string platformToken, User incomingUser)
+    public bool Register(User incomingUser, string tokenHash)
     {
+        bool validationResult = _platformInviteService.Validate(tokenHash);
+
+        // Bad token case
+        if (validationResult == false)
+        {
+            return false;
+        }
+
+        // Add user
+        _userRepository.Add(incomingUser);
+        _userRepository.Save();
+
+        // TODO: Prevent quering the invite a second time in the future
+        PlatformInvite? invite = _platformInviteRepository.FindOneBy(invite => invite.TokenHash == tokenHash);
+        invite.ReceiverId = incomingUser.Id;
+        invite.IsActive = false;
+        _platformInviteRepository.Save();
+
         return true;
     }
 
